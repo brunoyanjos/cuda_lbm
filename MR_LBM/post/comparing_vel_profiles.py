4c679dfac7e524ee0637c7c4bfb39218f16d1aad
brunoyanjos <bruno.yan.anjos@gmail.com>
2025-08-20 09:06:25 -0300

    updating python fle

--- MR_LBM/post/comparing_vel_profiles.py
+++ MR_LBM/post/comparing_vel_profiles.py
@@ -21,14 +21,29 @@ OMEGA_COARSE = 1.0 / TAU_COARSE
 
 ALPHA = OMEGA_COARSE / OMEGA_FINE * GRID_RATIO
 
+coarse_rho_path = "GRID/001/coarse_rho.bin"
+fine_rho_path = "GRID/001/fine_rho.bin"
+
 coarse_ux_path = "GRID/001/coarse_ux.bin"
 fine_ux_path = "GRID/001/fine_ux.bin"
 
 coarse_uy_path = "GRID/001/coarse_uy.bin"
 fine_uy_path = "GRID/001/fine_uy.bin"
 
+coarse_mxx_path = "GRID/001/coarse_mxx.bin"
+fine_mxx_path = "GRID/001/fine_mxx.bin"
+
 coarse_mxy_path = "GRID/001/coarse_mxy.bin"
 fine_mxy_path = "GRID/001/fine_mxy.bin"
+
+coarse_myy_path = "GRID/001/coarse_myy.bin"
+fine_myy_path = "GRID/001/fine_myy.bin"
+
+with open(coarse_rho_path, 'rb') as f:
+    coarse_rho = np.frombuffer(f.read(), dtype=np.float32)
+    
+with open(fine_rho_path, 'rb') as f:
+    fine_rho = np.frombuffer(f.read(), dtype=np.float32)
 
 with open(coarse_ux_path, 'rb') as f:
     coarse_ux = np.frombuffer(f.read(), dtype=np.float32)
@@ -48,17 +63,14 @@ with open(coarse_mxy_path, 'rb') as f:
 with open(fine_mxy_path, 'rb') as f:
     fine_mxy = np.frombuffer(f.read(), dtype=np.float32)
     
-calc_mxy = ALPHA * (coarse_mxy - coarse_ux * coarse_uy) + fine_ux[::2] * fine_uy[::2]
-    
-print(fine_ux)
-print(fine_ux[::2])
+calc_mxy_fine = (1 / ALPHA) * (coarse_mxy - coarse_ux * coarse_uy) + fine_ux[::2] * fine_uy[::2]
+calc_mxy_coarse = ALPHA * (fine_mxy[::2] - fine_ux[::2] * fine_uy[::2]) + coarse_ux * coarse_uy
             
 y_coarse = np.linspace(0, 1, NY_COARSE)
 y_fine = np.linspace(0, 1, NY_FINE)
 
-plt.plot(coarse_mxy , y_coarse, label = "coarse")
-plt.plot(fine_mxy , y_fine, label = "fine")
-plt.plot(calc_mxy , y_coarse, label = "calc_fine")
+plt.plot(coarse_uy , y_coarse, label = "coarse")
+plt.plot(fine_uy, y_fine, label = "fine")
 
 plt.legend(title="grid kind", loc='lower right')
 
